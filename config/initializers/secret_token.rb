7816cebae2ee9f95e86ecf63439c89d55efb4916
Cheech Church <cheechchurch2012@yahoo.ie>
2012-07-10 22:07:14 +0100

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplCmsBs3::Application.config.secret_token = '800db7cbecbc24530e95f48ba0360c4dc041b85e6ce52f369d09b5b815ea821c7bfcf6a26e4c6fb6ebd62a46d61106e85b84c0a8a8e6647de597e5cf23b9bcbb'
